1cfdbfe849e417b298d44bfe7bdd9e58ae91684a
Matthew Mc Colgan <Matthew Mc Colgan>
2017-04-13 12:32:02 +0100

    no change

---
+++ src/tests/Menu.java
@@ -0,0 +1,18 @@
+package tests;
+
+public class Menu{
+	public static void main(String [] args){
+		showMenu();
+		showThisMenu();
+	}
+	
+	public static void showMenu(){
+	System.out.println("This is a menu");
+	System.out.println("Its not a very good menu though.");
+	System.out.println("-------------------------------.");
+	}
+	
+	public static void showThisMenu(){
+		System.out.println("This is allll pointless!! blah blah");
+	}
+}
